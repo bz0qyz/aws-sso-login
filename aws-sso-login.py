4dd7048cfa590fe6ab0fa8c3139e685b99a48c94
Russ Cook <rcook@64byte.com>
2023-01-20 17:02:36 -0600

    Version 1.0.6

--- aws-sso-login.py
+++ aws-sso-login.py
@@ -12,7 +12,7 @@ from configparser import ConfigParser
 APP = {
     'name': 'aws-sso-login',
     'description': 'AWS SSO Login Manager',
-    'version': '1.0.5',
+    'version': '1.0.6',
 }
 DEFAULTS = {
     "config": {
